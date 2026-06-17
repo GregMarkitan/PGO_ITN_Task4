38af3bac28ac7afaad566258eb06e4a19325f646
GregMarkitan <s26387@pjwstk.edu.pl>
2026-06-17 13:46:11 +0200

    Implemented calculateTotalPremium() method

--- src/InsuranceOffice.java
+++ src/InsuranceOffice.java
@@ -9,5 +9,13 @@ class InsuranceOffice {
 	public void addPolicy(Policy policy) {
 		policies.add(policy);
 	}
+	public double calculateTotalPremium() {
+		double total = 0;
+		for (Policy policy : policies) {
+		total += policy.calculateFinalPremium();
+		}
+
+	return Math.round(total * 100.0) / 100.0;
+	}
 
 }
